7b00ca504426678e2041f602d843a2ab196c6067
James Youn <jamesyoun710@gmail.com>
2016-09-21 20:17:19 -0700

    Refactor nth prime solution

--- nth-prime/nth_prime.rb
+++ nth-prime/nth_prime.rb
@@ -1,12 +1,12 @@
 class Prime
   def self.nth(max_primes_count)
-    raise ArgumentError unless max_primes_count != 0
+    raise ArgumentError, '#nth only takes positive integers' unless max_primes_count >  0
 
     current_number = 2
     nth_prime = 2
 
     while max_primes_count > 0
-      if self.is_prime?(current_number)
+      if prime?(current_number)
         nth_prime = current_number
         max_primes_count -= 1
       end
@@ -16,10 +16,10 @@ class Prime
     nth_prime
   end
 
-  def self.is_prime?(num)
+  def self.prime?(num)
     max_check = Math.sqrt(num)
 
-    for i in 2..max_check
+    (2..max_check).each do |i|
       return false if num % i == 0
     end
 
