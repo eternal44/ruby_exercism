4b26deece6de610432121568831249b2d49eac1d
James Youn <jamesyoun710@gmail.com>
2016-09-26 01:19:11 -0700

    Refactor wordcount solution

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -4,24 +4,36 @@ end
 
 class Phrase
   def initialize(phrase)
-    @phrase = phrase
+    @phrase = phrase.downcase.split(/[\s,]/)
   end
 
   def word_count
     @phrase
-    .split(/[\s,]/)
     .select do |word|
-      /[a-zA-Z0-9]/.match(word).nil? ? false : true
+      contains_valid_char?(word)
     end
     .reduce(Hash.new(0)) do |memo, word|
-      word = word.split('').map do |letter|
-        /[a-zA-Z0-9']/.match(letter).nil? ? next : letter
-      end.join.downcase
-
-      word = word.slice(1..-2) if word[0] == "'"
+      word = format_word(word)
 
       memo[word] = memo[word] + 1
       memo
     end
   end
+
+  private
+
+  def contains_valid_char?(word)
+    /[a-zA-Z0-9']/.match(word).nil? ? false : true
+  end
+
+  def format_word(word)
+    word = word.split('').map do |letter|
+      /[a-zA-Z0-9']/.match(letter).nil? ? next : letter
+    end.join.downcase
+
+    word = word.slice(1..-2) if word[0] == "'"
+
+    word
+  end
 end
+
